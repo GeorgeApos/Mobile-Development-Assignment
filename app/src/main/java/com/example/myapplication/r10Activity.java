676edac6b0a6c0fd8e3843367255b691cd6b5f5b
Thomas <thomas4uh@gmail.com>
2022-06-06 15:43:52 +0300

    added dropdown filter menu

--- app/src/main/java/com/example/myapplication/r10Activity.java
+++ app/src/main/java/com/example/myapplication/r10Activity.java
@@ -6,10 +6,19 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class r10Activity extends AppCompatActivity {
+public class r10Activity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener{
+
+
+    private BottomNavigationView bottomNavigationView;
+    ImageButton showMenu;
 
 
     @Override
@@ -17,7 +26,56 @@ public class r10Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_r10);
 
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        showMenu = (ImageButton) findViewById(R.id.show_dropdown_menu);
+        showMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu dropDownMenu = new PopupMenu(getApplicationContext(),showMenu);
+                dropDownMenu.getMenuInflater().inflate(R.menu.r10_filter_menu, dropDownMenu.getMenu());
+                dropDownMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(getApplicationContext(), "You have clicked"+ menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                });
+                dropDownMenu.show();
+            }
+        });
 
     }
 
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        return false;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod=new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment fragment=null;
+
+                    switch (item.getItemId()){
+                        case R.id.home:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.ranking:
+                            fragment = new RankingFragment();
+                            break;
+                        case R.id.favourites:
+                            fragment = new FavouriteFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+
+                    return true;
+                }
+            };
 }
